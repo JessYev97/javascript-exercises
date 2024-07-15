f92360e9131fc30e8237a078138e723581ba8dbe
JessYev97 <jessmyeverino@gmail.com>
2024-07-15 11:55:19 -0500

    complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
-
-};
+const sumAll = function(minInteger, maxInteger) {
+  let finalSum = 0; 
+let larger = Math.max(minInteger, maxInteger);
+let smaller = Math.min(minInteger, maxInteger); 
+if (typeof minInteger !="number" || typeof maxInteger != "number") {
+  return "ERROR"; 
+} else if ((larger >= 0) && (smaller >= 0)) {
+  for (let i = smaller; i <= larger; i++) {
+    finalSum += i;
+  }
+} else return "ERROR"; 
+return finalSum; 
+}
 
 // Do not edit below this line
 module.exports = sumAll;
