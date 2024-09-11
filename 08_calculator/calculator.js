d80b70a7ee8ed39a2fb6659f21cf6a8f9168beef
JessYev97 <jessmyeverino@gmail.com>
2024-09-11 12:01:15 -0500

    finish code on tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -18,8 +18,16 @@ const power = function(a, b) {
 	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+
+    if (n === 0) { 
+        return 1; 
+    } 
+    else { 
+        return n * factorial( n - 1 ); 
+    
+} 
+
 };
 
 // Do not edit below this line
